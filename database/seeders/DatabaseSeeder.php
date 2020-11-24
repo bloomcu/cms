14e4e8d23a97f09664f6e439de322d13b71d8cd5
Ryan <Harmon>
2020-11-24 16:59:39 -0700

    Seed everything.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use App\Models\Layout;
 use App\Models\Block;
+use App\Models\Content;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +18,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Layout::factory()->count(3)
-            ->has(Block::factory()->count(5))
-            ->has(Post::factory()->count(10))
-            ->create();
+
+        /**
+         * Seed 100 posts
+         *
+         */
+        for ($i = 0; $i < 100; $i++) {
+
+            // Create post with layout and 5 blocks
+            $post = Post::factory()
+                ->for(Layout::factory()
+                    ->has(Block::factory()->count(5))
+                )
+                ->create();
+
+            // Create content for each block
+            foreach ($post->layout->blocks as $block) {
+                Content::factory()
+                    ->state([
+                        'block_id' => $block->id,
+                        'post_id' => $post->id
+                    ])
+                    ->create();
+            }
+        }
+
+        // Almost works
+        // Layout::factory()->count(100)
+        //     ->has(Block::factory()->count(5)
+        //         ->has(Content::factory()))
+        //     // ->has(Post::factory())
+        //     ->create();
+
     }
 }
