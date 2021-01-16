4ede140b67f3cfa4d78869a10c52b929f8ac22fc
Ryan <Harmon>
2021-01-15 18:29:12 -0700

    Add companies and company_users

---
+++ database/migrations/2020_11_23_215244_create_company_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('company_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_user');
+    }
+}
