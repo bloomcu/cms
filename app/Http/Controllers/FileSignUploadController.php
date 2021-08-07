456b2933756c14823d850ef4dc54277882b067ef
Ryan Harmon <heyharmon@gmail.com>
2021-08-07 15:42:49 -0600

    Don't alias

--- app/Http/Controllers/FileSignUploadController.php
+++ app/Http/Controllers/FileSignUploadController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Storage;
 
 use App\Models\Company;
 
-use Aws\S3\PostObjectv4 as S3PostObject;
+use Aws\S3\PostObjectv4;
 
 class FileSignUploadController extends Controller
 {
@@ -21,7 +21,7 @@ class FileSignUploadController extends Controller
         $filename = md5($request->name . microtime()) . '.' . $request->extension;
 
         // Create a signed upload request for AWS S3
-        $object = new S3PostObject(
+        $object = new PostObjectv4(
             // S3 Client interface
             Storage::disk('s3')->getAdapter()->getClient(),
 
