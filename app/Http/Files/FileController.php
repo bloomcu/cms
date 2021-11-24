51eb4ab92947d62a5ad5588017b651d971b55394
Ryan Harmon <heyharmon@gmail.com>
2021-11-24 01:24:22 -0700

    Quick fix on FileController

--- app/Http/Files/FileController.php
+++ app/Http/Files/FileController.php
@@ -9,6 +9,7 @@ use Cms\Domain\Organizations\Organization;
 
 use Cms\Http\Files\Resources\FileCollection;
 use Cms\Http\Files\Resources\FileResource;
+use Cms\Domain\Files\File;
 
 class FileController extends Controller
 {
@@ -31,10 +32,18 @@ class FileController extends Controller
      */
     public function store(Organization $organization, Request $request)
     {
-        $file = $organization->files()->firstOrCreate(
-            $request->only('path'),
-            $request->only(['name', 'size'])
-        );
+        // $file = $organization->files()->firstOrCreate(
+        //     $request->only('path'),
+        //     $request->only(['name', 'size'])
+        // );
+
+        $file = File::create([
+            'organization_id' => 1,
+            'user_id' => 1,
+            'name' => $request['name'],
+            'path' => $request['path'],
+            'size' => $request['size']
+        ]);
 
         return new FileResource($file);
     }
