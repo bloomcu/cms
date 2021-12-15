47f1f3092c65b0ec65924ebe532bdedbd8dc0930
Ryan Harmon <heyharmon@gmail.com>
2021-12-15 12:53:22 -0700

    Update FeatureResource.php

--- app/Http/Blocks/Resources/Data/FeatureResource.php
+++ app/Http/Blocks/Resources/Data/FeatureResource.php
@@ -4,6 +4,9 @@ namespace Cms\Http\Blocks\Resources\Data;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+use Cms\Http\Blocks\Resources\Image\ImageResource;
+use Cms\Http\Blocks\Resources\Button\ButtonCollection;
+
 class FeatureResource extends JsonResource
 {
     /**
@@ -15,8 +18,17 @@ class FeatureResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'title' => $this['title'],
-            'subtitle' => $this['subtitle']
+            'invert'     => isset($this['invert']) ? $this['invert'] : false,
+            'label'      => isset($this['label']) ? $this['label'] : 'The label',
+            'title'      => isset($this['title']) ? $this['title'] : 'The title',
+            'subtitle'   => isset($this['subtitle']) ? $this['subtitle'] : 'The subtitle',
+            'image'      => new ImageResource(isset($this['image']) ? $this['image'] : [
+                'src' => 'files/b91c769fd41d8f5091e0dc86e14e4ea4.jpg'
+            ]),
+            'buttons'    => new ButtonCollection(isset($this['buttons']) ? $this['buttons'] : [
+                ['text' => 'Button One', 'href' => ''],
+                ['text' => 'Button Two', 'href' => '']
+            ])
         ];
     }
 }
