d03c83fd4399c5b60d08d0c0a89c833f8bf9a0c3
Ryan Harmon <heyharmon@gmail.com>
2021-12-20 09:27:42 -0700

    Create HasSlug trait

--- app/App/Traits/HasSlug.php
+++ app/App/Traits/HasSlug.php
@@ -7,12 +7,39 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug {
 
-    protected static function bootHasSlug()
+    protected static function bootHasSlug(): void
     {
-        
         static::creating(function (Model $model) {
-            $model->slug = Str::slug($model->title);
+            $slug = Str::slug($model->title);
+            $model->slug = $model->makeSlugUnique($slug);
         });
+    }
 
+    protected function makeSlugUnique(string $slug): string
+    {
+        $original_slug = $slug;
+        $i = 2;
+
+        while ($this->otherRecordsExistWithSlug($slug)) {
+            $slug = $original_slug . '-' . $i++;
+        }
+
+        return $slug;
+    }
+
+    protected function otherRecordsExistWithSlug(string $slug): bool
+    {
+        $query = static::where('slug', '=', $slug);
+
+        if ($this->usesSoftDeletes()) {
+            $query->withTrashed();
+        }
+
+        return $query->exists();
+    }
+
+    protected function usesSoftDeletes(): bool
+    {
+        return in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this), true);
     }
 }
