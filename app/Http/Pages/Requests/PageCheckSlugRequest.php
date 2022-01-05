dbbc6267a189786e9e71ec692f6e75a37a85ac69
Ryan Harmon <heyharmon@gmail.com>
2022-01-05 14:06:00 -0700

    Add endpoint to check uniqueness of a slug.

---
+++ app/Http/Pages/Requests/PageCheckSlugRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cms\Http\Pages\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class PageCheckSlugRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'slug' => 'required|string',
+        ];
+    }
+
+    /**
+     * Return exception as json
+     *
+     * @return Exception
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
