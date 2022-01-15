d121a214b09e72d3d801e7ca0f63cfcc6533a93e
Ryan Harmon <heyharmon@gmail.com>
2022-01-14 17:22:42 -0700

    Update MenusSeeder.php

--- database/seeders/MenusSeeder.php
+++ database/seeders/MenusSeeder.php
@@ -19,51 +19,58 @@ class MenusSeeder extends Seeder
      */
     public function run()
     {
-        $menu = [
-            'title' => 'Primary Navigation',
-            'organization_id' => 1,
-            'items' => [
-                [
-                    'id' => 1,
-                    'title' => 'Accounts',
-                    'menu_id' => 1,
-                    'order' => 0,
-                    'children' => [
-                        ['title' => 'Checking', 'parent_id' => 1, 'menu_id' => 1, 'order' => 0],
-                        ['title' => 'Savings', 'parent_id' => 1, 'menu_id' => 1, 'order' => 1],
-                    ]
-                ],
-                [
-                    'id' => 4,
-                    'title' => 'Loans',
-                    'menu_id' => 1,
-                    'order' => 2,
-                    'children' => [
-                        ['title' => 'Vehicle', 'parent_id' => 4, 'menu_id' => 1, 'order' => 0],
-                        ['title' => 'Home', 'parent_id' => 4, 'menu_id' => 1, 'order' => 1],
-                        ['title' => 'Personal', 'parent_id' => 4, 'menu_id' => 1, 'order' => 2],
-                    ]
-                ],
-                [
-                    'id' => 8,
-                    'title' => 'Cards',
-                    'menu_id' => 1,
-                    'order' => 3,
-                    'children' => [
-                        ['title' => 'Credit', 'parent_id' => 8, 'menu_id' => 1, 'order' => 0],
-                        ['title' => 'Debit', 'parent_id' => 8, 'menu_id' => 1, 'order' => 1],
-                    ]
-                ],
+        $menus = [
+            [
+                'title' => 'Navbar',
+                'component' => 'navbar-1',
+                'organization_id' => 1,
             ]
         ];
 
-        Menu::create([
-            'title' => $menu['title'],
-            'organization_id' => $menu['organization_id'],
-        ]);
+        $items = [
+            [
+                'id' => 1,
+                'title' => 'Accounts',
+                'parent_id' => null,
+                'component' => 'dropdown-1',
+                'menu_id' => 1,
+                'order' => 0,
+                'children' => [
+                    ['id' => 2, 'title' => 'Checking', 'parent_id' => 1, 'component' => null, 'menu_id' => 1, 'order' => 0],
+                    ['id' => 3, 'title' => 'Savings', 'parent_id' => 1, 'component' => null, 'menu_id' => 1, 'order' => 1],
+                ]
+            ],
+            [
+                'id' => 4,
+                'title' => 'Loans',
+                'parent_id' => null,
+                'component' => 'dropdown-1',
+                'menu_id' => 1,
+                'order' => 2,
+                'children' => [
+                    ['id' => 5, 'title' => 'Vehicle', 'parent_id' => 4, 'component' => null, 'menu_id' => 1, 'order' => 0],
+                    ['id' => 6, 'title' => 'Home', 'parent_id' => 4, 'component' => null, 'menu_id' => 1, 'order' => 1],
+                    ['id' => 7, 'title' => 'Personal', 'parent_id' => 4, 'component' => null, 'menu_id' => 1, 'order' => 2],
+                ]
+            ],
+            [
+                'id' => 8,
+                'title' => 'Cards',
+                'parent_id' => null,
+                'component' => 'dropdown-1',
+                'menu_id' => 1,
+                'order' => 3,
+                'children' => [
+                    ['id' => 9, 'title' => 'Credit', 'parent_id' => 8, 'component' => null, 'menu_id' => 1, 'order' => 0],
+                    ['id' => 10, 'title' => 'Debit', 'parent_id' => 8, 'component' => null, 'menu_id' => 1, 'order' => 1],
+                ]
+            ]
+        ];
 
-        UpsertMenuItemsAction::execute(
-            $menu['items']
-        );
+        foreach ($menus as $menu) {
+            Menu::create($menu);
+        }
+
+        UpsertMenuItemsAction::execute($items);
     }
 }
