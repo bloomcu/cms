9cebcfba17592b752b71323116b323a4c1e4986d
Bryan <bryan.e.dev@gmail.com>
2022-01-15 15:26:24 -0600

    Rate Set fillable atts

--- app/Domain/Rates/Rate.php
+++ app/Domain/Rates/Rate.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Rate extends Model
 {
     use HasFactory;
+    public $fillable = ['row_id', 'col_id', 'data'];
 
     public function rate_table() {
         return $this->belongsTo('Cms\Domain\Rates\RateTable');
