04642c50c27778a64d424b393d2fcd788db790dc
Ryan Harmon <heyharmon@gmail.com>
2022-01-24 03:45:14 -0700

    Update ArticlesSeeder.php

--- database/seeders/ArticlesSeeder.php
+++ database/seeders/ArticlesSeeder.php
@@ -18,16 +18,12 @@ class ArticlesSeeder extends Seeder
         $articles = [
             [
                 'title' => 'Blank Article Blueprint',
-                // 'is_published' => false,
                 'property_id' => 1,
-                'category_id' => null,
                 'is_blueprint' => true,
             ],
             [
                 'title' => 'My first article!',
-                // 'is_published' => false,
                 'property_id' => 1,
-                'category_id' => 2,
                 'is_blueprint' => false,
             ]
         ];
