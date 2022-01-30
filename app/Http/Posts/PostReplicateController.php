af691c3ce648e0e710a752909ea43d17242b4d9f
Ryan Harmon <heyharmon@gmail.com>
2022-01-30 02:16:01 -0700

    Fix categories lazy load

--- app/Http/Posts/PostReplicateController.php
+++ app/Http/Posts/PostReplicateController.php
@@ -55,7 +55,7 @@ class PostReplicateController extends Controller
 
         return new PostResource(
             $newPost->load([
-                'category',
+                'categories',
                 'layout',
                 'layout.blocks'
             ])
