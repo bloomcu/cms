59a596a64b2e7d9da76404134409e42fb1299dc9
Ryan Harmon <heyharmon@gmail.com>
2022-02-09 12:27:38 -0700

    Update PublicPostController.php

--- app/Http/PublicApi/PublicPostController.php
+++ app/Http/PublicApi/PublicPostController.php
@@ -17,7 +17,7 @@ class PublicPostController extends Controller
     public function show(Property $property, Request $request)
     {
         // TODO: Create a request class for this        
-        $post = Post::where('url', $url)->firstOrFail();
+        $post = Post::where('url', $request['path'])->firstOrFail();
         
         if (!$post->isPublished()) {
             return response()->json(['message' => 'Not Found'], 404);
