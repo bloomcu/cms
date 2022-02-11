a1dffe53524fdcc2d5a43d70f65799ff2184ff2a
Ryan Harmon <heyharmon@gmail.com>
2022-02-11 00:42:40 -0700

    Version 0.0.1

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.0.0',
+    'version' => '0.0.1',
     
     /*
     |--------------------------------------------------------------------------
