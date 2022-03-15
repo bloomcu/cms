806c224b07b3aaf8d25797f95284634c4c8f54e8
Ryan Harmon <heyharmon@gmail.com>
2022-03-15 11:05:40 -0600

    v0.0.5

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.0.4',
+    'version' => '0.0.5',
     
     /*
     |--------------------------------------------------------------------------
